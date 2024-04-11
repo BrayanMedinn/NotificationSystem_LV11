7e283910a7a58cf9494171406fe0bee5872c04a1
Brayan Medina <brayanmedinn@gmail.com>
2024-04-11 18:29:05 -0500

    funcionalidad usuarios y categorias para suscribcion

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,39 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            Categorias
+        </h2>
+    </x-slot>
+
+    <div style="padding-top:3rem;">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                     Disponibles
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <section class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="p-6 flex flex-row justify-between">
+            <div class="relative" style="cursor:pointer">
+                <p class="absolute p-6">Deportes</p>
+                <!-- <img width="350" src="https://media.istockphoto.com/id/1354776457/vector/default-image-icon-vector-missing-picture-page-for-website-design-or-mobile-app-no-photo.jpg?s=612x612&w=0&k=20&c=w3OW0wX3LyiFRuDHo9A32Q0IUMtD4yjXEvQlqyYk9O4=" alt=""> -->
+                <img class="categoria-subscripcion" width="350" height="240" src="https://www.iberdrola.com/documents/20125/40948/mujer-deporte-alianza-exito-746x419.png/77d9c70c-fdd8-e9ad-0538-ad88a314ac0f?t=1701793959991" alt="deportes.jpg">
+                
+                
+            </div>
+            <div class="relative" style="cursor:pointer">
+                <p class="absolute p-6">Finanzas</p>
+                <!-- <img width="350" src="https://media.istockphoto.com/id/1354776457/vector/default-image-icon-vector-missing-picture-page-for-website-design-or-mobile-app-no-photo.jpg?s=612x612&w=0&k=20&c=w3OW0wX3LyiFRuDHo9A32Q0IUMtD4yjXEvQlqyYk9O4=" alt=""> -->
+                <img width="350" height="240" src="https://www.montepiedad.com.mx/storage/2022/04/consejos_para_cuidar_tu_salud_financiera.jpg" alt="finanzas.png">
+            </div>
+            <div class="relative" style="cursor:pointer;">
+                <p class="absolute p-6">Peliculas</p>
+                <!-- <img width="350" src="https://media.istockphoto.com/id/1354776457/vector/default-image-icon-vector-missing-picture-page-for-website-design-or-mobile-app-no-photo.jpg?s=612x612&w=0&k=20&c=w3OW0wX3LyiFRuDHo9A32Q0IUMtD4yjXEvQlqyYk9O4=" alt=""> -->
+                <img width="350" height="240" src="https://static.vecteezy.com/system/resources/previews/005/648/406/original/watching-movie-in-3d-glasses-flat-illustration-friends-students-enjoy-film-on-tv-set-with-popcorn-and-drinks-isolated-together-cartoon-characters-with-outline-elements-on-white-background-vector.jpg" alt="movie.jpg">
+            </div>
+        </div>
+    </section>
+</x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('index');
+    return view('auth.login');
 });
+
+Route::get('/register', function () {
+    return view('auth.register');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
